9d45ecb295107bdda92f11dbf436b818a34c226b
Mel <108138017+Melyssa2022@users.noreply.github.com>
2022-11-10 12:18:11 -0300

    10-11

---
+++ register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+ <!-- CSS only -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    
+</head>
+<body>
+    
+<div class="conteiner">
+<div class="wrapper">
+<h2>Cadastro</h2>
+<p>Por Favor, preencha este formulario para criar uma conta.</p>
+
+<form method="post">
+<div class="form-group">
+<label>Nome do usuário</label>
+<input type="text" name="username" class="form-control" value=" ">
+<span class="invalid-feedback"></span>
+</div>
+<div class="form-group">
+<label>Senha</label>
+<input type="password" name="password" class="form-control">
+<span class="invalid-feedback"></span>
+</div>
+<div class="form-group">
+<label>Confirme a Senha</label>
+<input type="password" name="password" class="form-control">
+<span class="invalid-feedback"></span>
+</div>
+<div class="form-group">
+<input type="submit" class="btn btn-primary" value="Criar Conta">
+<input type="reset" class="btn btn-secondary ml-2" value="Apagar Dados">
+</div>
+<p>já tem uma conta?Entre aqui</a>.</p>
+</form>
+</div>
+</div>
+
+
+</body>
+</html>
---
+++ reset-password.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redefinir senha</title>
+ <!-- CSS only -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    
+</head>
+<body>
+    
+<div class="conteiner">
+<div class="wrapper">
+<h2>Redefinir senha</h2>
+<p>Por Favor, preencha este formulario para redefinir sua senha.</p>
+
+<form method="post">
+<div class="form-group">
+<label>Nova senha</label>
+<input type="text" name="username" class="form-control" value=" ">
+<span class="invalid-feedback"></span>
+</div>
+<div class="form-group">
+<label>Confirme a senha</label>
+<input type="password" name="password" class="form-control">
+<span class="invalid-feedback"></span>
+</div>
+<div class="form-group">
+<input type="submit" class="btn btn-primary" value="Redefinir">
+<a class="btn btn-link ml-2">Cancelar</a>
+</div>
+</form>
+</div>
+</div>
+
+
+</body>
+</html>
